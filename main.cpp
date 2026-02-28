a0aa21381e6244d38036ec735d3b8044268d5901
jannatan61 <janatankamal8@gmail.com>
2026-02-28 20:58:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+void print(int n){
+if (n<1 || n>100){
+    //cout<<"the number has to be between 1 and 100"<<endl;
+    return;
+}
+
+cout<<n<<" ";
+print(n-1);
+}
+int main()
+{
+    int n;
+    cin>> n;
+    print(n);
+    return 0;
+}
+
+
